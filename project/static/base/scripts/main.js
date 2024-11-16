e1b65356bc884c1995cd7953791d55889cd0100e
quramboyev <quramboyev17@gmail.com>
2024-11-16 20:10:03 +0500

    сделан слайдер

--- project/static/base/scripts/main.js
+++ project/static/base/scripts/main.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {
+window.onscroll = function () {
     const scrollBtn = document.getElementById("scrollUpBtn");
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         scrollBtn.classList.add("show");
@@ -7,37 +7,64 @@ window.onscroll = function() {
     }
 };
 
-document.getElementById("scrollUpBtn").onclick = function() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+document.getElementById("scrollUpBtn").onclick = function () {
+    window.scrollTo({top: 0, behavior: "smooth"});
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-        const toggleButton = document.querySelector(".nav-toggle");
-        const mobileNav = document.querySelector(".nav-mobile");
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.querySelector(".nav-toggle");
+    const mobileNav = document.querySelector(".nav-mobile");
 
-        toggleButton.addEventListener("click", function() {
-            mobileNav.classList.toggle("show");
-        });
+    toggleButton.addEventListener("click", function () {
+        mobileNav.classList.toggle("show");
     });
+});
 
-$('.slider').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1
-                }
+
+
+
+$('.slider-gallery').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
             }
-        ]
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+// navbar
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.querySelector('nav');
+    const newsSection = document.querySelector('#news');
+
+    window.addEventListener('scroll', function () {
+        const navbarHeight = navbar.offsetHeight;
+        const newsTop = newsSection.getBoundingClientRect().top;
+
+        if (newsTop + 700 <= navbarHeight) {
+            navbar.classList.add('non-sticky');
+        } else {
+            navbar.classList.remove('non-sticky');
+        }
     });
+});
+
+
