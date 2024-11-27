331fb8cdbd2526d1bf2c073826711d173e5c6f0a
quramboyev <quramboyev17@gmail.com>
2024-11-27 13:01:41 +0500

    oxirgi

--- project/assets/base/scripts/base.js
+++ project/assets/base/scripts/base.js
@@ -50,3 +50,58 @@ function filterEvents(type, btn) {
 function showCalendar() {
     alert("Показать календарь - функция не реализована");
 }
+
+
+
+// slides
+const slides = document.querySelector('.slides');
+let prevIndex = 0;
+let currentIndex = 0;
+const totalSlides = slides.children.length;
+
+const keys = {
+    right: 'ArrowRight',
+    left: 'ArrowLeft'
+}
+
+const theme = {
+    '--clr-white': ['white', 'black']
+}
+
+const moveNext = () => {
+    prevIndex = currentIndex;
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateSlider();
+}
+
+const movePrev = () => {
+    prevIndex = currentIndex;
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    updateSlider();
+}
+
+document.querySelector('.next').onclick = moveNext;
+document.querySelector('.prev').onclick = movePrev;
+
+document.onkeydown = (event) => {
+    switch (event.key) {
+        case keys.right: {
+            moveNext()
+            break
+        }
+        case keys.left: {
+            movePrev()
+            break
+        }
+    }
+}
+
+setInterval(() => {
+    prevIndex = currentIndex;
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateSlider()
+}, 3000)
+
+function updateSlider() {
+    slides.style.transform = `translateX(-${currentIndex * slides.scrollWidth / totalSlides}px)`;
+}
