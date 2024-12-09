4ae37ec0c9eda83b026d07baf223770cf2a46955
Asadbek <ahikmatullayev024@gmail.com>
2024-12-09 22:01:00 +0500

    [fix] home.admin

--- project/apps/home/admin.py
+++ project/apps/home/admin.py
@@ -4,4 +4,4 @@ from parler.admin import TranslatableAdmin
 
 
 admin.site.register(DocumentModel, TranslatableAdmin)
-admin.site.register(ImageModel, TranslatableAdmin)
+admin.site.register(ImageModel)
